55484ba41300d61240cd866cf3765598483bc396
shoko furushita <shktkt219@gmail.com>
2016-11-11 16:22:26 -0500

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,21 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+	it('should filter favoritefood correctly', function(){
+		 var fakeList = [{
+			 name: 'Bob',
+			 favoriteFood: 'Mint'
+		 },{
+			 name: 'Cath',
+			 favoriteFood: 'Chocolate'
+		 },{
+			 name: 'Tom',
+			 favoriteFood: 'Hot pot'
+		 }];
+
+		 var results = $filter('favoriteFood')(fakeList, 'Mint');
+
+		 expect(results.length).toBe(1);
+		 expect(results[0].name).toBe('Bob');
+	});
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,10 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+	it('should remove all vowels', function(){
+		var results = $filter('removeAllVowels')('abstractwords');
+
+		expect(results).toEqual('bstrctwrds');
+	});
+
 });
